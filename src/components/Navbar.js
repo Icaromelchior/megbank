7b9d1a1f5f96d64d8b114b948518f6d8526478f1
Icaro Melchior <icaro_barbosa@outlook.com>
2022-05-07 00:27:15 -0300

    feat(vavbar):end-navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Navbar, Nav, Container,ButtonGroup, Button, NavDropdown} from 'react-bootstrap';
+import { Container, Navbar, Nav,ButtonGroup, Button, NavDropdown} from 'react-bootstrap';
 import './Navbar.scss';
+import logo from '../essets/logo.svg';
 
 const Navgation = () => (
-    <Navbar bg="light" expand="lg">
-  <Container fluid>
-    <Navbar.Brand href="#">Megbank</Navbar.Brand>
+  <Navbar variant= 'dark' expand="lg">
+    <Container>
+    <Navbar.Brand href="#">
+      <img
+          src={logo}
+          height="30"
+          className="d-inline-block align-top"
+          alt="Megbank logo"
+        />
+    </Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
@@ -17,14 +25,14 @@ const Navgation = () => (
         <Nav.Link href="#fac">FAQ</Nav.Link>
       </Nav>
         <ButtonGroup aria-label="Basic example">
-          <Button variant="outline-secondary"><NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
+          <Button variant="outline-light"><NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
         </NavDropdown></Button>
-          <Button variant="outline-secondary">Abra sua conta</Button>
+          <Button variant="outline-light">Abra sua conta</Button>
         </ButtonGroup>
     </Navbar.Collapse>
   </Container>
